eb5d023caab814400cd0957aad8ee621048a8ec7
WonderSi <kirillkozlov00@gmail.com>
2024-12-04 02:41:02 +0700

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.attendancetrackingapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.attendancetrackingapp to javafx.fxml;
+    exports org.example.attendancetrackingapp;
+}
